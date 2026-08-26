6c2f0901a35d88c1de958e7bff57ef26913d5444
Pedro Aguiar <115799705+Tiep7@users.noreply.github.com>
2026-08-26 14:26:08 -0300

    Update ArquivoVideo.java

--- Construtores-copia_de_objetos/Arquivos_Multimidia/ArquivoVideo.java
+++ Construtores-copia_de_objetos/Arquivos_Multimidia/ArquivoVideo.java
@@ -1,16 +1,15 @@
 package multimidia;
 public class ArquivoVideo extends ArquivoMultimidia {
-    private String arquivo;
     int largura, altura;
     public ArquivoVideo(String nomeArquivo, int largura, int altura) {
         super(nomeArquivo);
-        arquivo = nomeArquivo;
         this.largura = largura;
         this.altura = altura;
     }
     @Override
     public String toString() {
-        return "Video: " + arquivo  + " (" + largura + " x " + altura + ")";
+        String video = String.format("Video: %s (%d x %d)", getNomeArquivo(), largura, altura);
+        return video;
     }
     
 }
