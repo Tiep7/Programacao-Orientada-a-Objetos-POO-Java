fde541351614627d17c488b464fc3776ead92b1d
Pedro Aguiar <115799705+Tiep7@users.noreply.github.com>
2026-08-26 14:34:45 -0300

    Create PerfilRedeSocial.java

---
+++ Construtores-copia_de_objetos/Perfil-de-Usuario/PerfilRedeSocial.java
@@ -0,0 +1,16 @@
+package usuarios;
+public class PerfilRedeSocial extends PerfilUsuario {
+    private String mensagem;
+    public PerfilRedeSocial(String nome, String login) {
+        super(nome, login);
+        this.mensagem = "Sem mensagem";
+    }
+    public PerfilRedeSocial(String nome, String login, String mensagemPaginaInicial) {
+        super(nome, login);
+        this.mensagem = mensagemPaginaInicial;
+    }
+    public String getPerfil() {
+        return getNome() + " " + mensagem;
+    }
+    
+}
