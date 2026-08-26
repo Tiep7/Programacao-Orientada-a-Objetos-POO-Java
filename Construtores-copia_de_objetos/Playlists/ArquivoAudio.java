893635c5af9cfc6ca8cca3eb2e1bef5e58e43abc
Pedro Aguiar <115799705+Tiep7@users.noreply.github.com>
2026-08-26 14:51:31 -0300

    Create ArquivoAudio.java

---
+++ Construtores-copia_de_objetos/Playlists/ArquivoAudio.java
@@ -0,0 +1,14 @@
+package multimidia;
+public class ArquivoAudio extends ArquivoMultimidia{
+    private boolean audioHD;
+    public ArquivoAudio(String nomeArquivo, boolean audioHD) {
+        super(nomeArquivo);
+        this.audioHD = audioHD;
+    }
+    @Override
+    public String toString() {
+        String audio = String.format("Audio: %s (HD = %s)", getNomeArquivo(), audioHD);
+        return audio;
+    }
+    
+}
