4131c4029c017a670a9e47162bdb7945e664da53
Pedro Aguiar <115799705+Tiep7@users.noreply.github.com>
2026-08-27 14:36:43 -0300

    Update PerfilUsuario.java

--- Construtores-copia_de_objetos/Perfil-de-Usuario/PerfilUsuario.java
+++ Construtores-copia_de_objetos/Perfil-de-Usuario/PerfilUsuario.java
@@ -1,3 +1,18 @@
+/*Escreva as classes PerfilRedeSocial e PerfilAnonimo (pacote usuarios) conforme descrito a seguir:
+
+
+Classe PerfilUsuario: 
+
+Classe PerfilRedeSocial (subclasse de PerfilUsuario):
+
+PerfilRedeSocial(String nome, String login): construtor que inicializa nome e login. Esses atributos devem ser atribuídos na superclasse PerfilUsuario. Além disso, a mensagem para a página inicial neste construtor deve ser "Sem mensagem".
+PerfilRedeSocial(String nome, String login, String mensagemPaginaInicial): construtor que inicializa nome e login. Esses atributos devem ser atribuídos na superclasse PerfilUsuario. Esse construtor recebe também uma mensagem para a página inicial.
+String getPerfil(): retorna uma string no formato: "<nome> <mensagemPaginaInicial>"
+ 
+Classe PerfilAnonimo (subclasse de PerfilUsuario):
+ 
+PerfilAnonimo(): construtor que inicializa o nome com a string "Anonimo". Esse atributo deve ser atribuído na superclasse PerfilUsuario.*/
+
 package usuarios;
 
 public class PerfilUsuario {
