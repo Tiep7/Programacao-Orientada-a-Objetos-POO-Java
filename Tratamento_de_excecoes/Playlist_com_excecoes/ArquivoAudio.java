d24ccf2ee89c35f32fda435710a46d764be85504
Pedro Aguiar <115799705+Tiep7@users.noreply.github.com>
2026-08-29 18:41:13 -0300

    Create ArquivoAudio.java

---
+++ Tratamento_de_excecoes/Playlist_com_excecoes/ArquivoAudio.java
@@ -0,0 +1,13 @@
+package multimidia;
+public class ArquivoAudio extends ArquivoMultimidia {
+    private boolean audioHD;
+    public ArquivoAudio (String nomeArquivo, int tamanho, boolean audioHD) {
+        super(nomeArquivo, tamanho);
+        this.audioHD = audioHD;
+    }
+    @Override
+    public String toString() {
+        String audio = String.format("Audio: %s (%d)", getNomeArquivo(), getTamanhoArquivo());
+        return audio;
+    }
+}
